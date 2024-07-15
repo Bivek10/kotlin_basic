ebd37e1ae9e29cebca538164e2aec0a5be7546c3
Bivek10 <bibekkarki662@gmail.com>
2024-07-15 15:24:39 +0545

    learn: infix function

--- app/src/main/java/com/bv/kotlin_basic/Main.kt
+++ app/src/main/java/com/bv/kotlin_basic/Main.kt
@@ -10,6 +10,21 @@ fun main(){
     printMessageWithPrefix("actual message", "hello prefix")
     printMessageWithPrefix(prefix = "prefix first", message = "test")
 
+    //infix function
+    val person1= Person("Bivek")
+    val person2= Person("Ram")
+
+    person1 likes  person2
+    // infix extension fun on int
+    infix fun Int.times(str:String) = str.repeat(this)
+    println(0 times "bye")
+
+    infix fun String.onto(other:String)= Pair(this, other)
+
+    val myValue = "ram".onto("sita")
+
+    println(myValue)
+
 }
 
 
---
+++ app/src/main/java/com/bv/kotlin_basic/infex_fun.kt
@@ -0,0 +1,12 @@
+package com.bv.kotlin_basic
+
+class Person(val name:String){
+    private val likedPerson =  mutableListOf<Person>()
+
+    infix  fun likes(other:Person){
+        println("liked person after $likedPerson")
+        likedPerson.add(other)
+        println("liked person before $likedPerson")
+    }
+
+}
