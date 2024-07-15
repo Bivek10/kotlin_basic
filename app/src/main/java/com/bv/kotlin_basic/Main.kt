76439d9c4337e9eb28ec8e14556eedda1d1dbda4
Bivek10 <bibekkarki662@gmail.com>
2024-07-15 13:27:51 +0545

    learn: default parameter values and named argument

--- app/src/main/java/com/bv/kotlin_basic/Main.kt
+++ app/src/main/java/com/bv/kotlin_basic/Main.kt
@@ -2,4 +2,27 @@ package com.bv.kotlin_basic
 
 fun main(){
     println("Hello world")
+    printMessage("Hello Bivek")
+    println("sum:: ${sum(10, 50)}")
+    println("Multiply:: ${multiply(10, 10)}")
+
+    printMessageWithPrefix(message = "test name argument")
+    printMessageWithPrefix("actual message", "hello prefix")
+    printMessageWithPrefix(prefix = "prefix first", message = "test")
+
+}
+
+
+fun printMessage (message:String): Unit{
+    println("message $message")
+}
+
+fun sum (a:Int, b:Int): Int{
+    return  a+b
+}
+
+fun multiply(x:Int, y:Int)= x*y;
+
+fun printMessageWithPrefix(message: String, prefix:String ="default"){
+  println("$prefix $message")
 }
